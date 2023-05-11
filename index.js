3c9718f4ebd0e1833edc9b66e097507790a91faf
Magowntown <amagown@gmail.com>
2023-05-10 21:58:45 -0400

    JS function Error messages corrected for input deletion. tailwindcss attribute values corrected for error message styling issue.

--- index.js
+++ index.js
@@ -1,75 +1,71 @@
-// function verifyPassword() {
-//   var lowerCaseLetters = /[a-z]/g;
-//   let pw = document.getElementById("psw").value;
-//   //check empty password field
-//   if (pw == "") {
-//     document.getElementById("passwordError").innerHTML =
-//       "**Fill the password please!";
-//     return false;
-//   }
-
-//   //minimum password length validation
-//   if (pw.length < 8) {
-//     document.getElementById("passwordError").innerHTML =
-//       "**Password length must be atleast 8 characters";
-//     return false;
-//   }
-
-//   //maximum length of password validation
-//   if (pw.length > 15) {
-//     document.getElementById("passwordError").innerHTML =
-//       "**Password length must not exceed 15 characters";
-//     return false;
-//   } else {
-//     alert("Password is correct");
-//   }
-
-//   if (document.getElementById("psw").value.match(lowerCaseLetters)) {
-//     document.getElementById("letter").classList.remove("invalid");
-//     document.getElementById("letter").classList.add("valid");
-//   } else {
-//     document.getElementById("letter").classList.remove("valid");
-//     document.getElementById("letter").classList.add("invalid");
-//   }
-// }
-
 let pswCheck = {
   lowerCaseLetters: /[a-z]/g,
   upperCaseLetters: /[A-Z]/g,
+  numbers: /[0-9]/g,
   eightCheck: function (psw) {
     if (psw.length >= 8) {
-      let greenlight = document.createElement("span");
-      greenlight.innerHTML = "Password length is correct";
-      greenlight.style.color = "green";
-      if (!document.getElementById("passBox").contains(greenlight)) {
-        document.getElementById("passBox").appendChild(greenlight);
-      } else {
-        
-    }
+      document.getElementById("pswErrLength").innerHTML =
+        "Password length is correct";
+      document.getElementById("pswErrLength").style.color = "green";
     } else {
-      document.getElementById("passwordError").innerHTML =
-        "**Password length must be atleast 8 characters";
+      document.getElementById("pswErrLength").innerHTML =
+        "**Password length must be at least 8 characters";
+      document.getElementById("pswErrLength").style.color = "red";
     }
   },
   numberCheck: function (psw) {
     if (psw.match(this.numbers)) {
-      document.getElementById("passwordError").innerHTML =
+      document.getElementById("pswErrNum").innerHTML =
         "Password contains a number";
+      document.getElementById("pswErrNum").style.color = "green";
     } else {
-      document.getElementById("passwordError").innerHTML =
+      document.getElementById("pswErrNum").innerHTML =
         "Password does not contain a number";
+      document.getElementById("pswErrNum").style.color = "red";
     }
   },
+  lowerCheck: function (psw) {
+    if (psw.match(this.lowerCaseLetters)) {
+      document.getElementById("pswErrLowCase").innerHTML =
+        "Password contains a lower case letter";
+      document.getElementById("pswErrLowCase").style.color = "green";
+    } else {
+      document.getElementById("pswErrLowCase").innerHTML =
+        "Password does not contain a lower case letter";
+      document.getElementById("pswErrLowCase").style.color = "red";
+    }
+  },
+  upperCheck: function (psw) {
+    if (psw.match(this.upperCaseLetters)) {
+      document.getElementById("pswErrUpCase").innerHTML =
+        "Password contains an upper case letter";
+      document.getElementById("pswErrUpCase").style.color = "green";
+    } else {
+      document.getElementById("pswErrUpCase").innerHTML =
+        "Password does not contain an upper case letter";
+      document.getElementById("pswErrUpCase").style.color = "red";
+    }
+  },
+  clearErrors: function() {
+    document.getElementById("pswErrLength").innerHTML = "";
+    document.getElementById("pswErrNum").innerHTML = "";
+    document.getElementById("pswErrLowCase").innerHTML = "";
+    document.getElementById("pswErrUpCase").innerHTML = "";
+  }
 };
 
 function pswVerification() {
   let psw = document.getElementById("psw").value;
-
-  if (psw == "") {
-    document.getElementById("passwordError").innerHTML =
+  
+  if (psw === "") {
+    pswCheck.clearErrors();
+    document.getElementById("pswErrorBlank").innerHTML =
       "**Fill the password please!";
   } else {
-    pswCheck.eightCheck(psw);
+    document.getElementById("pswErrorBlank").innerHTML = "";
+    pswCheck.lowerCheck(psw);
+    pswCheck.upperCheck(psw);
     pswCheck.numberCheck(psw);
+    pswCheck.eightCheck(psw);
   }
 }
